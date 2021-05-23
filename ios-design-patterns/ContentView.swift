85b6453cff3dbf4029c29832d4850b6284880d52
Akshay Gajarlawar <akshaygajarlawar@gmail.com>
2021-05-23 19:18:56 +0530

    initial commit

---
+++ ios-design-patterns/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-design-patterns
+//
+//  Created by Akshay Gajarlawar on 23/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-design-patterns/ios_design_patternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_design_patternsApp.swift
+//  ios-design-patterns
+//
+//  Created by Akshay Gajarlawar on 23/05/21.
+//
+
+import SwiftUI
+
+@main
+struct ios_design_patternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
